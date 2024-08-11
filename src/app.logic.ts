e1ea62136cca640ccd6603acbb3339bb651d2f0f
Daniel Benavides <danibenavi1907@gmail.com>
2024-08-10 22:10:26 -0600

    Refactorizando y trabajando con casos de uso

--- src/app.logic.ts
+++ src/app.logic.ts
@@ -1,15 +1,24 @@
 import fs from 'fs'
+import { yarg } from './config/plugins/args.plugin'
+
+console.log(yarg)
+
+const { b: base, l: limit, s: showTable } = yarg
 
 const headerMessage = `Tabla del 5 \n\n`
+
 let resultado = ''
-let base = 5
-for (let i = 1; i <= 10; i++) {
+
+for (let i = 1; i <= limit; i++) {
   resultado += `${base} x ${i} = ${base * i}\n`
 }
 
 const outPutMessage = headerMessage + resultado
 
-console.log(outPutMessage)
+if (showTable) {
+  console.log(outPutMessage)
+}
+
 const pathOutPut = 'outputs'
 
 fs.mkdirSync(pathOutPut, { recursive: true })
